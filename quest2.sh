ff0b18ee2f6c1782edb06ecf35dd538690d06384
will <56336577+will0999@users.noreply.github.com>
2021-08-10 13:30:39 -0300

    Add files via upload

---
+++ quest2.sh
@@ -0,0 +1,16 @@
+read -p 'diretorio 1: ' d1
+read -p 'diretorio 2: ' d2
+read -p 'diretorio 3: ' d3
+read -p 'diretorio 4: ' d4
+read -p 'diretorio 5: ' d5
+echo "----d1----"
+ls $d1
+echo "----d2----"
+ls $d2
+echo "----d3----"
+ls $d3
+echo "----d3----"
+ls $d4
+echo "----d4----"
+ls $d5
+echo "----d5----"
